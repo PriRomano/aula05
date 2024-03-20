c45687c88b0346da7e54df0f00437e4d65e6636b
Priscila <priscilaromano2004@hotmail.com>
2024-03-20 12:14:35 -0300

    meu terceiro commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, mundo!");
+        System.out.println(x:"Olá, mundo! 20/03/2024");
+        
     }
 }
